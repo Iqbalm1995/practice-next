6cb25c69b02fbf1806b4e28e77773d3a21541186
iqbalm1995 <iqbalm1995@gmail.com>
2025-01-22 11:11:07 +0700

    code head

--- src/app/(pages)/users/page.tsx
+++ src/app/(pages)/users/page.tsx
@@ -6,8 +6,32 @@ import {
 } from "@/app/components/headerContent";
 import SidebarWithHeader from "@/app/components/sidebar";
 import { fakeDataUser, userData } from "@/app/types/userInterface";
-import { Box, Card, CardBody, CardHeader, Text } from "@chakra-ui/react";
-import { ColumnDef, PaginationState } from "@tanstack/react-table";
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Heading,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  PaginationState,
+  useReactTable,
+} from "@tanstack/react-table";
 import { use, useEffect, useMemo, useState } from "react";
 
 const HeaderDataContent: HeaderContentProps = {
@@ -17,9 +41,11 @@ const HeaderDataContent: HeaderContentProps = {
 
 function SettingsPage() {
   const [dataUsers, setdataUsers] = useState<userData[]>([]);
+  const [totalData, settotalData] = useState<number>(0);
 
   useEffect(() => {
     setdataUsers(fakeDataUser);
+    settotalData(fakeDataUser.length);
   }, [dataUsers]);
 
   // Setup Pagination Tabel
@@ -62,12 +88,67 @@ function SettingsPage() {
     []
   );
 
+  // setup tanstack react-table
+  const table = useReactTable({
+    // Data
+    data: dataUsers,
+    columns: columns,
+    pageCount: totalData,
+    state: {
+      pagination,
+    },
+    // Pipeline
+    onPaginationChange: setPagination,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    // manual
+    manualFiltering: true,
+    manualPagination: true,
+  });
+
   return (
     <SidebarWithHeader>
       <HeaderContent {...HeaderDataContent} />
       <Card>
         <CardHeader>{HeaderDataContent.titleName}</CardHeader>
         <CardBody>
+          <Flex w={"full"} justifyContent={"space-between"}>
+            <TableContainer w={"full"}>
+              <Table variant="simple">
+                {/* HEAD TABLE CODE */}
+                <Thead>
+                  {table.getHeaderGroups().map((headerGroup: any, idx) => (
+                    <Tr key={idx} bg={"primary.50"}>
+                      {headerGroup.headers.map((header: any) => {
+                        return (
+                          <Th
+                            key={header.id}
+                            colSpan={header.colSpan}
+                            color={"primary.800"}
+                          >
+                            <Heading as="h5" size="sm">
+                              {flexRender(
+                                header.column.columnDef.header,
+                                header.getContext()
+                              )}
+                            </Heading>
+                          </Th>
+                        );
+                      })}
+                    </Tr>
+                  ))}
+                </Thead>
+                {/* BODY TABLE CODE */}
+                <Tbody>
+                  <Tr>
+                    <Td>inches</Td>
+                    <Td>millimetres (mm)</Td>
+                    <Td isNumeric>25.4</Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </Flex>
           <Box>
             {/* Code buat cek data json */}
             <pre>{JSON.stringify(dataUsers, null, 2)}</pre>
